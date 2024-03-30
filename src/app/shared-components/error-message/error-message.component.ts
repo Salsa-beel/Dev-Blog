0439b39515b20f5ff740176be65a3569db84400c
salsabeel <123001325+Salsa-beel@users.noreply.github.com>
2024-03-30 20:51:53 +0200

    added Error Handling and try again button

--- src/app/shared-components/error-message/error-message.component.ts
+++ src/app/shared-components/error-message/error-message.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogServiceService } from 'src/app/blog-service/blog-service.service';
+import { BlogPost } from 'src/app/models/blog-post.model';
 
 @Component({
   selector: 'app-error-message',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./error-message.component.css']
 })
 export class ErrorMessageComponent implements OnInit {
+  blogPosts: BlogPost[]=[];
+  isLoading: boolean=false;
+  isError: boolean=false;
 
-  constructor() { }
+
+  constructor(private blogservice:BlogServiceService) { }
 
   ngOnInit() {
+    this.fetchBlogPosts();
   }
 
+  fetchBlogPosts(): void {
+    this.isLoading = true;
+    this.blogservice.getPosts().subscribe(
+      (posts: BlogPost[]) => {
+        this.blogPosts = posts;
+        this.isLoading = false;
+      },
+      (error: any) => {
+        console.error('Error fetching blog posts:', error);
+        this.isError = true;
+        this.isLoading = false;
+      }
+    );
+  }
+
+  retryFetchBlogPosts(): void {
+    // Reset error state
+    this.isError = false;
+    this.isLoading = true;
+    // Retry fetching blog posts
+    this.fetchBlogPosts();
+  }
 }
